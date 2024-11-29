c558d2807d8140c3bbc9be9c237e0d5f02152154
vishnu kumar <kumarvishnu1619@gmail.com>
2024-11-29 23:31:31 +0530

    tool widget

--- lib/pages/tab-widgets/ToolsScreen.dart
+++ lib/pages/tab-widgets/ToolsScreen.dart
@@ -11,17 +11,34 @@ class ToolsScreen extends StatefulWidget {
 class _ToolsScreenState extends State<ToolsScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: GridView.count(crossAxisCount: 3,children: [
-      Card(child: Text("Merge Pdf"),),
-      Card(child: Text("mergePdf"),),
-      Card(child: Text("reorderPdf"),),
-      Card(child: Text("splitPdf"),),
-      Card(child: Text("pdfToJpg"),),
-      Card(child: Text("imageToPdf"),),
-      Card(child: Text("pageNumbers"),),
-      Card(child: Text("rotatePdf"),),
-      Card(child: Text("unprotectPdf"),),
-      Card(child: Text("protectpdf"),),
-    ],));
+    return SafeArea(child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(crossAxisCount: 3,
+        children: [
+          PdfTool(name: "Merge Pdf",),
+          PdfTool(name: "Reorder Pdf",),
+          PdfTool(name: "Split Pdf",),
+          PdfTool(name: "Pdf To Jpg",),
+          PdfTool(name: "Image To Pdf",),
+          PdfTool(name: "Page Numbers",),
+          PdfTool(name: "Rotate Pdf",),
+          PdfTool(name: "Unprotect Pdf",),
+          PdfTool(name: "Protect Pdf",),
+      ],),
+    ));
+  }
+}
+
+class PdfTool extends StatelessWidget {
+  final String name;
+
+  const PdfTool({
+    super.key,
+    required this.name
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(child: Card(child: Padding(padding: EdgeInsets.all(8),child: Text(name),),elevation: 0.1,borderOnForeground: true));
   }
 }
